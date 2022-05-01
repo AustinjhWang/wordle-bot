f8019734c4ef9c97afbdb1d6203de5e2d4a1adec
unknown <wang.austin.2000@gmail.com>
2022-05-01 16:37:31 -0700

    refactor and add comments

--- wordle.py
+++ wordle.py
@@ -6,15 +6,16 @@ from selenium.webdriver.support import expected_conditions as EC
 import time 
 import random
 
-
+# create list from words.txt
 words_file = open("words.txt", "r")
 words_string = words_file.read() # a string
 words_list = words_string.split("\n") # returns list
 words_file.close()
-#print(words_list)
-DNE_letters = set()
+
+# keep track of correct letters (green), possible letters (yellow), and letters that do not exist (grey)
 Correct_letters = []
 Possible_letters = set()
+DNE_letters = set()
 
 def done(feedback):
       for item in feedback:
@@ -32,46 +33,63 @@ def guess(feedback, previous_guess):
             elif item == "present":
                   Possible_letters.add((previous_guess[index], index))
             index = index + 1
+      
+      # check for absent after adding correct letters to avoid grey green
+      # only add to DNE if letter is not already in correct/possible letters
       index = 0
-      # it is always adding because a != (a,2)
       for item in feedback:
             if item == "absent":
                   letter = previous_guess[index]
-                  correct_letters_list = [letter for item in Correct_letters for letter in item]
-                  possible_letters_list = [letter for item in Possible_letters for letter in item]
+                  
+                  # convert list of tuples into list
+                  correct_letters_list = [element for tuple in Correct_letters for element in tuple]
+                  possible_letters_list = [element for tuple in Possible_letters for element in tuple]
+
                   if letter not in correct_letters_list and letter not in possible_letters_list:
                         DNE_letters.add(letter)
+
             index = index + 1
+      
+      # loop through wordlist and remove all invalid words
+      remove_set = set()
+      for word in words_list:
+
+            for letter, index in Correct_letters:
+                  if word[index] != letter:
+                        remove_set.add(word)
+
+            for letter in DNE_letters:
+                  if letter in word: 
+                        remove_set.add(word)
+
+            for letter, index in Possible_letters:
+                  if letter not in word:
+                        remove_set.add(word)
+                  if word[index] == letter:
+                        remove_set.add(word)
+
+      for word in remove_set:
+            words_list.remove(word)
+      
+      #feedback
       print("correct")
       print(Correct_letters)
       print("possible")
       print(Possible_letters)
       print("not possible")
       print(DNE_letters)
-      remove_set = set()
-      for word in words_list:
-            for letter, index in Correct_letters:
-                  if word[index] != letter:
-                        remove_set.add(word)
-            if any(letter in word for letter in DNE_letters):
-                  remove_set.add(word)
-            for letter, index in Possible_letters:
-                  if letter not in word:
-                        remove_set.add(word)
-                  if word[index] == letter:
-                        remove_set.add(word)
-      for word in remove_set:
-            words_list.remove(word)
-      print(len(words_list))
+      print("number of possible words left: " + str(len(words_list))) 
+
       word = random.choice(words_list)
       return word
 
-print(len(words_list))
 driver = webdriver.Chrome()
 driver.get("https://www.nytimes.com/games/wordle/index.html")
 #driver.get("https://wordle.berknation.com/")
 
+guess_number = 1
 first_guess = "salet"
+previous_guess = first_guess
 
 webdriver.ActionChains(driver).click().perform()
 webdriver.ActionChains(driver).send_keys(first_guess + Keys.ENTER).perform()
@@ -82,8 +100,7 @@ for tile in row:
       feedback.append(tile.get_attribute("evaluation"))
       print(tile.get_attribute("evaluation"))
 
-guess_number = 1
-previous_guess = first_guess
+
 
 while not done(feedback) and guess_number < 6:
       time.sleep(2)
